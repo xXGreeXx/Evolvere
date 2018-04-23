169859559bcc5472b529f7bd8d9a6a075fbe0399
xXGreeXx <lrhude.elijah@gmail.com>
2018-04-23 14:45:09 -0400

    Worked on a lot
    -Added base textures
    -Made terrain
    -Wrote in day/night script
    -Added camera scroll/zoom

---
+++ Evolvere A Geneticist's Sandbox/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    //update
+	void Update () {
+
+        //scroll camera in and out
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f && Camera.main.fieldOfView > 5)
+        {
+            Camera.main.fieldOfView -= 5;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f && Camera.main.fieldOfView < 95)
+        {
+            Camera.main.fieldOfView += 5;
+        }
+
+        //move camera
+        if (Input.GetMouseButton(0))
+        {
+            transform.LookAt(new Vector3(210, 0, 290));
+            transform.RotateAround(new Vector3(210, 0, 290), Vector3.up, Input.GetAxis("Mouse X") * 2);
+        }
+    }
+}
---
+++ Evolvere A Geneticist's Sandbox/Assets/Scripts/DayNightHandlerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightHandlerScript : MonoBehaviour {
+
+    //define global variables
+    public float speed = 0.025F;
+
+	//update
+	void FixedUpdate () {
+        GameObject.Find("Sun").transform.Rotate(new Vector3(0, 15 * speed, 0));
+	}
+}
